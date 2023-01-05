736624d05ce3fa77f18b5c96272f0d77ead9de32
caiomcg <caiomcg@gmail.com>
2023-01-05 16:31:06 -0300

    fix: arrows not point to the correct side

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -2,9 +2,11 @@ require('lualine').setup {
   options = {
     theme = 'auto',
     icons_enabled = true,
-    component_separators = {left = '', right = ''},
+    component_separators = {left = '', right = ''},
     section_separators = {left = '', right = ''},
     extensions = {'nvim-tree'},
     -- 
+    -- component_separators = {left = '', right = ''},
+    -- section_separators = {left = '', right = ''},
   },
 }
