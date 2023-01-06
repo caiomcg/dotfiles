b63528dada9395d6ac2f6a58abb928d2a14deb63
caiomcg <caiomcg@gmail.com>
2023-01-06 01:32:12 -0300

    feat: fix inconsistent install of packer

--- .config/nvim/lua/caiomcg/init.lua
+++ .config/nvim/lua/caiomcg/init.lua
@@ -1,3 +1,29 @@
-require("caiomcg.packer")
-require("caiomcg.settings")
-require("caiomcg.keymap")
+local caiomcg = {}
+
+function caiomcg.can_inject()
+    local caiomcgpacker = require("caiomcg.packer")
+    return caiomcgpacker.trytoconfigure()
+end
+
+function caiomcg.inject_vim_settings()
+    require("caiomcg.settings")
+    require("caiomcg.keymap")
+end
+
+function caiomcg.inject_plugin_settings()
+    require("caiomcg.Comment")
+    require("caiomcg.fugitive")
+    require("caiomcg.gitsigns")
+    require("caiomcg.harpoon")
+    require("caiomcg.indent_blankline")
+    require("caiomcg.keymap")
+    require("caiomcg.lsp")
+    require("caiomcg.lualine")
+    require("caiomcg.neodev")
+    require("caiomcg.nvim-tree")
+    require("caiomcg.nvim-treesitter")
+    require("caiomcg.telescope")
+    require("caiomcg.undotree")
+end
+
+return caiomcg
