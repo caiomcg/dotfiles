3b508d8adcdbcf37b0a3b587f35f514eeb16c2ad
caiomcg <caiomcg@gmail.com>
2024-06-13 12:53:01 -0300

    feat: add obsidian plugin support

--- .config/nvim/lua/caiomcg/keymap.lua
+++ .config/nvim/lua/caiomcg/keymap.lua
@@ -67,3 +67,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+
+vim.opt_local.conceallevel = 2
+
+-- navigate to vault
+vim.keymap.set("n", "<leader>oo", ":cd ~/caiomcg_vault<cr>")
+--
+-- convert note to template and remove leading white space
+vim.keymap.set("n", "<leader>on", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>")
+
+-- strip date from note title and replace dashes with spaces
+-- must have cursor on title
+vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
+--
+-- search for files in full vault
+vim.keymap.set("n", "<leader>os", ":Telescope find_files search_dirs={\"~/caiomcg_vault/notes\"}<cr>")
+vim.keymap.set("n", "<leader>oz", ":Telescope live_grep search_dirs={\"~/caiomcg_valut/notes\"}<cr>")
+-- delete file in current buffer
+vim.keymap.set("n", "<leader>odd", ":!rm '%:p'<cr>:bd<cr>")
